188f04ce0960a70e2b0aa650214865cf70d6ad69
Rahul Agarwal <mail.rainbox@gmail.com>
2023-12-28 15:41:55 +0530

    Spring Dependency Injection Tested

---
+++ src/main/java/com/example/Vaccination/TimeAndLocation.java
@@ -0,0 +1,44 @@
+package com.example.Vaccination;
+
+/**
+  TimeAndLocation class is a class which a user can use to set its appointment details
+  from the console.This class should be injected as a dependency into the selected user.
+  To complete this class you need perform the following tasks:
+   
+        Tasks:
+ 1.Add common attributes as mentioned below:
+  a. String timeSlot
+  b. String location
+  c. String date
+ 2. Implement below function based on the given description:
+    a. setDetails(String timeSlot, String location, String date): This method takes timeSlot, location, and date as input parameters.
+    b. getDetails(): This method returns a string containing the timeslot, location, and date for example: "12:00 AM at New Delhi on 2023-05-22". 
+
+**/ 
+public class TimeAndLocation {
+	private String timeSlot;
+	public void setTimeSlot(String timeSlot) {
+		this.timeSlot = timeSlot;
+	}
+
+	private String location;
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	private String date;
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+//	void setDetails(String timeSlot, String location, String date) {
+//		this.timeSlot = timeSlot;
+//		this.location = location;
+//		this.date = date;
+//	}
+	
+	String getDetails() {
+		return this.timeSlot+" at "+this.location+" on "+this.date;
+	}
+}
